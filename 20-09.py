d37b31369f4e221b22d4c0f575b74e79411b0e58
blaja02 <144525529+blaja02@users.noreply.github.com>
2023-09-20 12:00:46 +0000

    blahitka

---
+++ 20-09.py
@@ -0,0 +1,30 @@
+print (3+3)
+
+print (2//2)
+print  (2**10)
+
+jednicka=1
+trojka=3
+spatnecislo=5
+
+print (jednicka+trojka)
+
+#for promenna in [1,2,3,4,5 ]:
+#    print (promenna-spatnecislo)
+
+#if prommena-spatnecislo in [-1,-2,-3,-4,-5 ]:
+#    opraveny_vyraz = (prommena-spatnecislo) * -1
+
+
+for promenna in [1,2,3,4,5 ]:
+    print (promenna-spatnecislo)
+
+soucet=0
+
+for cislo in range (1,1000):
+    soucet=soucet+cislo
+
+print (soucet)
+first=cislo
+second=1001-cislo
+
